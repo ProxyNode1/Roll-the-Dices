2aa4e63c5278d0f16f3913c664e58da0aa5b03bc
Vix-Laptop <vsthakur451@gmail.com>
2026-03-10 21:52:48 +0100

    created a function for button animation

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-console.log("Test Passed");
+//console.log("Test Passed");
 
 let resultText = document.querySelector(".container > h1");
 let diceImgs = document.querySelectorAll(".container img");
+let button = document.querySelectorAll(".container button");
 
 let player1Dice = diceImgs[0];
-console.log(player1Dice);
+//console.log(player1Dice);
 
 let player2Dice = diceImgs[1];
-console.log(player2Dice);
+//console.log(player2Dice);
 
 
 //const rollButton = document.querySelector("input;");
 //const potentiallyUnsafeString = "alert('You got mail')";
 //rollButton.setAttribute("onclick", potentiallyUnsafeString);
 
-function rollDices() {    
+function animateButton() {
+    //disable the button    
 
-    console.log("Rolling the dice");
+    /* setTimeout(function() {
+    // enable the button
+    // animate it going up
+    }) */
+
+}
+
+function rollDices() {
+
+    animateButton();
+
+    //console.log("Rolling the dice");
     
     let player1DiceNr = Math.floor((Math.random() * 6)) + 1; //random nr between 1-6
     //console.log(player1DiceNr);
-    player1Dice.src = "./images/dice" + player1DiceNr + ".png";
+    player1Dice.src = "./assets/images/dice" + player1DiceNr + ".png";
 
     let player2DiceNr = Math.floor((Math.random() * 6)) + 1; //random nr between 1-6
-    console.log(player2DiceNr);
-    player2Dice.src = "./images/dice" + player2DiceNr + ".png";
+    //console.log(player2DiceNr);
+    player2Dice.src = "./assets/images/dice" + player2DiceNr + ".png";
 
     if (player1DiceNr > player2DiceNr) {
-        console.log("player1DiceNr greater");
+        //console.log("player1DiceNr greater");
         resultText.textContent = "🚩 Player 1 Win";
     }
 
     else if (player2DiceNr > player1DiceNr) {
-        console.log("player2DiceNr greater");
+        //console.log("player2DiceNr greater");
         resultText.textContent = "🚩 Player 2 Win";
     }
 
     else {
-        console.log("player1DiceNr and player2DiceNr are equal");
+        //console.log("player1DiceNr and player2DiceNr are equal");
         resultText.textContent = "Draw";
     }
 
